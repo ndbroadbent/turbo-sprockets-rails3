6c1ee7e182008ef869ad8a855483ffa04319156a
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-25 09:42:57 +1300

    Handle @imports in Sass and Less by just running the processors. It only takes an extra second or so, and is better (IMO) than a lot more monkeypatching.

--- lib/sprockets/unprocessed_asset.rb
+++ lib/sprockets/unprocessed_asset.rb
@@ -1,44 +1,26 @@
 module Sprockets
   class UnprocessedAsset < AssetWithDependencies
-    def replace_imports(template_with_leading_underscore = true)
-      @source.gsub!(/^@import ["']([^"']+)["'];?/) do |match|
-        begin
-          if template_with_leading_underscore
-            template = "_#{$1}"
-          else
-            template = $1
-          end
-          pathname = @environment.resolve(template)
-          asset = UnprocessedAsset.new @environment, template, pathname
-          # Replace imported template with the unprocessed asset contents.
-          asset.to_s
-        rescue Sprockets::FileNotFound
-          match
-        end
-      end
-    end
-
-
     def initialize(environment, logical_path, pathname)
       super
 
       @environment = environment
       context = environment.context_class.new(environment, logical_path, pathname)
       attributes = environment.attributes_for(pathname)
-      processors = attributes.processors
 
       # Remove all engine processors except ERB to return unprocessed source file
-      processors -= (attributes.engines - [Tilt::ERBTemplate])
+      allowed_engines = [Tilt::ERBTemplate]
+      processors = attributes.processors - (attributes.engines - allowed_engines)
 
       @source = context.evaluate(pathname, :processors => processors)
 
-      # Include SASS imports
-      if defined?(Sass::Rails::ScssTemplate) && attributes.processors.include?(Sass::Rails::ScssTemplate)
-        replace_imports(true)  # Template has leading underscore
-      end
-      # Include Less imports
-      if defined?(Less::Rails::LessTemplate) && attributes.processors.include?(Less::Rails::LessTemplate)
-        replace_imports(false)  # Template has no leading underscore
+      # If the source contains any '@import' directives, add Sass and Less processors.
+      # (@import is quite difficult to handle properly without processing.)
+      if @source.include?("@import")
+        # Process Sass and Less files, since @import is too tricky to handle properly.
+        allowed_engines << Sass::Rails::ScssTemplate if defined?(Sass::Rails::ScssTemplate)
+        allowed_engines << Less::Rails::LessTemplate if defined?(Less::Rails::LessTemplate)
+        processors = attributes.processors - (attributes.engines - allowed_engines)
+        @source = context.evaluate(pathname, :processors => processors)
       end
 
       build_required_assets(environment, context,  :process => false)
