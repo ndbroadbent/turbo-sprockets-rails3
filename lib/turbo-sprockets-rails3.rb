77501ac1b9950922fb3d1e249fb0b74b5136543a
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-31 23:03:34 +1300

    Require sprockets before overrides

--- lib/turbo-sprockets-rails3.rb
+++ lib/turbo-sprockets-rails3.rb
@@ -6,6 +6,7 @@ module Sprockets
   autoload :AssetWithDependencies, "sprockets/asset_with_dependencies"
 end
 
+require 'sprockets'
 Dir[File.expand_path('../turbo-sprockets/sprockets_overrides/*.rb', __FILE__)].each do |f|
   require f
 end
